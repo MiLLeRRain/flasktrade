df0fd297de51c18491bce6883164145f96f4890d
MiLLeRRain <xxdezz@gmail.com>
2022-01-14 19:22:16 +1300

    lightsail done

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def play():
     return render_template('play.html')
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
